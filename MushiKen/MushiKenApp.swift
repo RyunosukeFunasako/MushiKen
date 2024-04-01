ea99d4be28bb5ab69e6e0d6f6f8a8efc2748e276
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2024-04-01 23:30:05 +0900

    Initial Commit

---
+++ MushiKen/MushiKenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MushiKenApp.swift
+//  MushiKen
+//
+//  Created by 船迫龍之介 on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct MushiKenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
