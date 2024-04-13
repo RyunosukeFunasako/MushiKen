414ccda732b12874a7689a524e78a9f6067cac12
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2024-04-13 17:47:36 +0900

    add: BGMの追加

--- MushiKen/ContentView.swift
+++ MushiKen/ContentView.swift
@@ -1,6 +1,31 @@
 import SwiftUI
+import AVFoundation
+import UIKit
+
+// BGMと効果音を再生するクラス
+class SoundPlayer: NSObject {
+    // BGMの音源データを読み込み
+    let bgmData = NSDataAsset(name: "PlayfulRivals")!.data
+    
+    // BGM用プレイヤーの変数
+    var bgmPlayer: AVAudioPlayer!
+    
+    // BGMを再生する関数
+    func playBGM() {
+        do {
+            bgmPlayer = try AVAudioPlayer(data: bgmData)
+            bgmPlayer.numberOfLoops = -1  // BGMを無限にループさせる
+            bgmPlayer.play()
+        } catch {
+            print("BGMでエラーが発生しました！")
+        }
+    }
+}
 
 struct ContentView: View {
+    // SoundPlayerのインスタンスを作成
+    let soundPlayer = SoundPlayer()
+    
     var body: some View {
         VStack {
             NavigationStack {
@@ -14,12 +39,15 @@ struct ContentView: View {
                         NavigationLink {
                             MushikenView()
                         } label: {
-                            Text("虫拳をなす！")
+                            Text("虫拳をはじめる！")
                                 .font(.title)
                         }
                     }
                 }
             }
+        }
+        .onAppear {
+            soundPlayer.playBGM()
         }
     }
 }
